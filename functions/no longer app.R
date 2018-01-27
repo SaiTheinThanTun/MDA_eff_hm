9d3be52d8fb2f34a967e6ed5036a64cc3e0c308e
SaiTheinThanTunOnMac <theinthantun.sai@gmail.com>
2018-01-27 14:30:52 +0000

    No longer app.R before turning into pure function

--- functions/no longer app.R
+++ functions/no longer app.R
@@ -282,96 +282,67 @@ colnames(GMSout0R) <- colnames(GMSoutiR) <- outLab
 #below is just testing the app to see if it's still working#####
 ################################################################
 
-plotR <- function()
-{
-  GMSout0<-GMSout0R
-  
-  GMSouti<-GMSoutiR
-  
-  times<-GMSout0[,1]
-  #0
-  clinmonth_det0<-cbind(GMSout0[,2],GMSouti[,2])
-  clinmonth_tot0<-cbind(GMSout0[,3],GMSouti[,3])
-  prevalence0<-cbind(GMSout0[,4],GMSouti[,4])
-  
-  runin<-(2016-startyear)/dt
-  
-  finclin0<-max(clinmonth_tot0[(runin:length(clinmonth_det0[,1])),])
-  finprev0<-max(prevalence0[(runin:length(prevalence0[,1])),])
-  
-  #1
-  clinmonth_det1<-cbind(GMSout0[,5],GMSouti[,5])
-  clinmonth_tot1<-cbind(GMSout0[,6],GMSouti[,6])
-  prevalence1<-cbind(GMSout0[,7],GMSouti[,7])
-  
-  runin<-(2016-startyear)/dt
-  
-  finclin1<-max(clinmonth_tot1[(runin:length(clinmonth_det1[,1])),])
-  finprev1<-max(prevalence1[(runin:length(prevalence1[,1])),])
-  
-  # PLOTTING
-  par(mfrow=c(1,2), cex=1.5)
-  
-  #0
-  maxy<-max(finclin0,finclin1,API/12)
-  x<-times[(runin:length(clinmonth_det0[,1]))]
-  y1<-clinmonth_det0[runin:length(clinmonth_det0[,1]),2]
-  y2<-clinmonth_tot0[runin:length(clinmonth_tot0[,1]),2]
-  
-  plot(x,y1, type='l',lty=1,col=rgb(1,0,0,alpha=0.1),xlab = "Time",ylab="incidence per 1000 per month",main="Monthly cases per 1000 population",ylim=c(0,maxy),lwd=2)
-  lines(x,y2, type='l',lty=1,col=rgb(1,0,0,alpha=0.1),lwd=2)
-  
-  polygon(c(x,rev(x)),c(y2,rev(y1)),col=rgb(1,0,0,alpha=0.1),border=NA)
-  
-  y1<-clinmonth_det1[runin:length(clinmonth_det1[,1]),2]
-  y2<-clinmonth_tot1[runin:length(clinmonth_tot1[,1]),2]
-  lines(x,y1, type='l',lty=1,col=rgb(0,0,1,alpha=0.4),lwd=2)
-  lines(x,y2, type='l',lty=1,col=rgb(0,0,1,alpha=0.4),lwd=2)
-  
-  polygon(c(x,rev(x)),c(y2,rev(y1)),col=rgb(0,0,1,alpha=0.4),border=NA)
-  
-  lines(c(2018,2018),c(-maxy,2*maxy),col="dark grey",lty=3,lwd=2)
-  
-  abline(h=API/12,col="dark blue",lty=1,lwd=1)
-  abline(h=1/12,col="red",lty=3,lwd=3)
-  maxy<-max(finprev0,finprev1)
-  plot(times[(runin:length(prevalence0[,1]))],prevalence0[(runin:length(prevalence0[,1])),2], type='l',lty=1,col=rgb(1,0,0,alpha=0.25),xlab = "Time",ylab="% prevalence",main="Predicted true prevalence",ylim=c(0,maxy),lwd=6)
-  lines(times[(runin:length(prevalence1[,1]))],prevalence1[(runin:length(prevalence1[,1])),2], type='l',lty=1,col=rgb(0,0,1,alpha=0.6),xlab = "Time",ylab="% prevalence",main="Predicted true prevalence",ylim=c(0,maxy),lwd=6)
-  lines(c(2018,2018),c(-maxy,2*maxy),col="dark grey",lty=3,lwd=2)
-  
-}
+# plotR <- function()
+# {
+#   GMSout0<-GMSout0R
+#   
+#   GMSouti<-GMSoutiR
+#   
+#   times<-GMSout0[,1]
+#   #0
+#   clinmonth_det0<-cbind(GMSout0[,2],GMSouti[,2])
+#   clinmonth_tot0<-cbind(GMSout0[,3],GMSouti[,3])
+#   prevalence0<-cbind(GMSout0[,4],GMSouti[,4])
+#   
+#   runin<-(2016-startyear)/dt
+#   
+#   finclin0<-max(clinmonth_tot0[(runin:length(clinmonth_det0[,1])),])
+#   finprev0<-max(prevalence0[(runin:length(prevalence0[,1])),])
+#   
+#   #1
+#   clinmonth_det1<-cbind(GMSout0[,5],GMSouti[,5])
+#   clinmonth_tot1<-cbind(GMSout0[,6],GMSouti[,6])
+#   prevalence1<-cbind(GMSout0[,7],GMSouti[,7])
+#   
+#   runin<-(2016-startyear)/dt
+#   
+#   finclin1<-max(clinmonth_tot1[(runin:length(clinmonth_det1[,1])),])
+#   finprev1<-max(prevalence1[(runin:length(prevalence1[,1])),])
+#   
+#   # PLOTTING
+#   par(mfrow=c(1,2), cex=1.5)
+#   
+#   #0
+#   maxy<-max(finclin0,finclin1,API/12)
+#   x<-times[(runin:length(clinmonth_det0[,1]))]
+#   y1<-clinmonth_det0[runin:length(clinmonth_det0[,1]),2]
+#   y2<-clinmonth_tot0[runin:length(clinmonth_tot0[,1]),2]
+#   
+#   plot(x,y1, type='l',lty=1,col=rgb(1,0,0,alpha=0.1),xlab = "Time",ylab="incidence per 1000 per month",main="Monthly cases per 1000 population",ylim=c(0,maxy),lwd=2)
+#   lines(x,y2, type='l',lty=1,col=rgb(1,0,0,alpha=0.1),lwd=2)
+#   
+#   polygon(c(x,rev(x)),c(y2,rev(y1)),col=rgb(1,0,0,alpha=0.1),border=NA)
+#   
+#   y1<-clinmonth_det1[runin:length(clinmonth_det1[,1]),2]
+#   y2<-clinmonth_tot1[runin:length(clinmonth_tot1[,1]),2]
+#   lines(x,y1, type='l',lty=1,col=rgb(0,0,1,alpha=0.4),lwd=2)
+#   lines(x,y2, type='l',lty=1,col=rgb(0,0,1,alpha=0.4),lwd=2)
+#   
+#   polygon(c(x,rev(x)),c(y2,rev(y1)),col=rgb(0,0,1,alpha=0.4),border=NA)
+#   
+#   lines(c(2018,2018),c(-maxy,2*maxy),col="dark grey",lty=3,lwd=2)
+#   
+#   abline(h=API/12,col="dark blue",lty=1,lwd=1)
+#   abline(h=1/12,col="red",lty=3,lwd=3)
+#   maxy<-max(finprev0,finprev1)
+#   plot(times[(runin:length(prevalence0[,1]))],prevalence0[(runin:length(prevalence0[,1])),2], type='l',lty=1,col=rgb(1,0,0,alpha=0.25),xlab = "Time",ylab="% prevalence",main="Predicted true prevalence",ylim=c(0,maxy),lwd=6)
+#   lines(times[(runin:length(prevalence1[,1]))],prevalence1[(runin:length(prevalence1[,1])),2], type='l',lty=1,col=rgb(0,0,1,alpha=0.6),xlab = "Time",ylab="% prevalence",main="Predicted true prevalence",ylim=c(0,maxy),lwd=6)
+#   lines(c(2018,2018),c(-maxy,2*maxy),col="dark grey",lty=3,lwd=2)
+#   
+# }
 
 #scenario
-png(filename=paste('results_homo_cov/plot_',gsub("\\:","",Sys.time()),'.png',sep=''), height= 1600, width=4800, units= "px", res=300) #if(...=="png"){png(file)} else if(...=="pdf"){pdf(file)}
-plotR()
-dev.off()
-# output$MODEL <- renderPlot({
-#   plotR()
-# })
-
-# output$MODEL2 <- renderPlot({
-#   plotR2()
-# })
-
-# output$downloadplot <- downloadHandler(
-#   filename = function(){paste('MalMod_',gsub("\\:","",Sys.time()),'.png',sep='')},
-#   content = function(file) {
-#     png(filename=file, height= 1600, width=4800, units= "px", res=300) #if(...=="png"){png(file)} else if(...=="pdf"){pdf(file)}
-#     plotR()
-#     dev.off()
-#   })
-
-# tableContentR <- ({
-#   tmp <- c(scenario_iR, API, parametersR)
-#   tmp2 <- cbind(ParLabel[,1], tmp, ParLabel[,2], names(tmp))
-#   colnames(tmp2) <- c("Name","Value","Unit","VarName")
-#   tmp2
-# })
-
-#   output$downloadTable <- downloadHandler(
-#     filename = function(){paste('MalMod_',gsub("\\:","",Sys.time()),'.csv',sep='')},
-#     content = function(file) {
-#       write.csv(tableContentR(), file, row.names = FALSE)
-#     })
-# }
+# png(filename=paste('results_homo_cov/plot_',gsub("\\:","",Sys.time()),'.png',sep=''), height= 1600, width=4800, units= "px", res=300) #if(...=="png"){png(file)} else if(...=="pdf"){pdf(file)}
+# plotR()
+# dev.off()
 

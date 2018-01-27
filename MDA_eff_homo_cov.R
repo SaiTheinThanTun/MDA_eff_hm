14a42672701bb3af868ceea126dd24641438c53e
SaiTheinThanTunOnMac <theinthantun.sai@gmail.com>
2018-01-27 20:29:15 +0000

    First graph produced

--- MDA_eff_homo_cov.R
+++ MDA_eff_homo_cov.R
@@ -12,6 +12,7 @@ library(shiny)
 library(TSA)
 library(Rcpp)
 library(stringr)
+library(lattice)
 sourceCpp("functions/modGMS.cpp")
 source("functions/no longer app.R")
 
@@ -22,6 +23,7 @@ testfor2i <- rep(1:80,each=100)
 testfor2 <- cbind(testfor2j,testfor2i)
 # colnames(testfor2) <- c('homogen','cmda_2')
 colnames(testfor2) <- NULL
+successwithin <- 6
 
 result <- list()
 #scenario
@@ -202,3 +204,43 @@ for(i in 1:80){
 #outside of 'for' loop
 #write.table(GMSoutiR[,1],'parameters/times.csv', col.names = 'time', row.names = FALSE)
 saveRDS(result, paste('results_homo_cov/results_',gsub("\\:","",Sys.time()),'.rds',sep=''))
+#scenario
+
+#Analysing the data list 'results_.rds'####
+#readRDS
+
+timeVector <- read.csv('parameters/times.csv')
+MDAstart <- which(timeVector==(2018+tm_1/12))
+
+#draft
+#result[[1]][MDAstart:(MDAstart+successwithin),]
+#sum(result[[1]][MDAstart:(MDAstart+successwithin),1])>0
+
+village1 <- sapply(result, function(x){
+  sum(x[MDAstart:(MDAstart+successwithin),1])>0
+})
+village2 <- sapply(result, function(x){
+  sum(x[MDAstart:(MDAstart+successwithin),2])>0
+})
+
+#putting into matrix
+v1m <- matrix(as.numeric(village1),nrow=80,ncol=100, byrow=TRUE)
+#heatmap(v1m, Rowv=NA, Colv = NA)
+#v1md <- as.data.frame(as.numeric(village1),nrow=80,ncol=100, byrow=TRUE)
+levelplot(t(v1m))
+
+v2m <- matrix(as.numeric(village2),nrow=80,ncol=100, byrow=TRUE)
+#heatmap(v2m, Rowv=NA, Colv = NA)
+levelplot(t(v2m))
+
+v12m <- matrix(as.numeric(village1),nrow=80,ncol=100, byrow=TRUE)+matrix(as.numeric(village2),nrow=80,ncol=100, byrow=TRUE)
+#heatmap(v12m, Rowv=NA, Colv = NA, col=heat.colors(3))
+#write.csv(v12m,'results_homo_cov/v12m.csv')
+
+new.palette=colorRampPalette(c("red","black"),space="rgb")
+levelplot(t(v12m), col.regions=new.palette, xlab="% of homogeniety", ylab="% of MDA coverage in village 2", main="No. of villages reaching below elimination threshold")
+
+png(paste('results_homo_cov/homogeniety_MDAcoverage',gsub("\\:","",Sys.time()),'.png',sep=''),height= 1600, width=1800, units= "px", res=300)
+levelplot(t(v12m), col.regions=new.palette, xlab="% of homogeniety", ylab="% of MDA coverage in village 2", main="No. of villages reaching below elimination threshold")
+dev.off()
+
